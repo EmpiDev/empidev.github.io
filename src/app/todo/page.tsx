e973fa87717f94b8167dae243d327ef4d9e731d2
EmpirisDev <empiriss@gmail.com>
2024-09-30 13:13:04 +0200

    Refactor Todo component, improve styling and functionality

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from 'react';
 
 interface TodoItem {
@@ -41,28 +42,43 @@ export default function Todo() {
     };
 
     return (
-        <div>
-            <h1>Todo</h1>
-            <input
-                type="text"
-                value={newTodo}
-                onChange={(e) => setNewTodo(e.target.value)}
-                placeholder="Add a new todo"
-            />
-            <button onClick={addTodo}>Add</button>
-            <ul>
-                {todos.map(todo => (
-                    <li key={todo.id}>
-                        <span
-                            style={{ textDecoration: todo.done ? 'line-through' : 'none' }}
-                            onClick={() => toggleTodo(todo.id)}
-                        >
-                            {todo.text}
-                        </span>
-                        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-                    </li>
-                ))}
-            </ul>
+        <div className="flex justify-center items-center h-screen bg-gray-100">
+            <div className="bg-white p-6 rounded-lg shadow-lg w-96">
+                <h1 className="text-2xl font-bold mb-4 text-center">Todo</h1>
+                <div className="flex mb-4">
+                    <input
+                        type="text"
+                        value={newTodo}
+                        onChange={(e) => setNewTodo(e.target.value)}
+                        placeholder="Add a new todo"
+                        className="flex-1 p-2 border border-gray-300 rounded mr-2"
+                    />
+                    <button
+                        onClick={addTodo}
+                        className="bg-green-500 text-white p-2 rounded hover:bg-green-600"
+                    >
+                        Add
+                    </button>
+                </div>
+                <ul className="list-none p-0">
+                    {todos.map(todo => (
+                        <li key={todo.id} className="flex justify-between items-center p-2 border-b border-gray-300">
+                            <span
+                                className={`flex-1 cursor-pointer ${todo.done ? 'line-through text-gray-500' : ''}`}
+                                onClick={() => toggleTodo(todo.id)}
+                            >
+                                {todo.text}
+                            </span>
+                            <button
+                                onClick={() => deleteTodo(todo.id)}
+                                className="text-red-500 hover:text-red-700"
+                            >
+                                Delete
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
